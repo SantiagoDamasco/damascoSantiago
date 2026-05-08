62097e6ac166e5bc86fda0077cc567d86d04fb3a
SantiagoDamasco <santiagodamasco2006@gmail.com>
2026-05-08 20:03:36 -0300

    Eliminar carpeta Desktop

--- Desktop/GUIAS/GUIA02/RICK AND MORTY/JSRAM/ejercicio02.js
+++
@@ -1,9 +0,0 @@
-//Mostrá el nombre y el estado(status) de los primeros 5 personajes del array results. Formato: "Rick Sanchez — Alive"
-
-fetch("https://rickandmortyapi.com/api/character")
-    .then(response => response.json())
-    .then(data => {
-        const primerosCinco = data.results.slice(0, 5);
-        const resultado = primerosCinco.map(character => `${character.name} - ${character.status}`).join("<br>");
-        document.getElementById("resultado").innerHTML = resultado;
-    })
--- Desktop/GUIAS/GUIA02/RICK AND MORTY/JSRAM/ejercicio03.js
+++
@@ -1,9 +0,0 @@
-//Del array results, filtrá solo los que tienen status === "Alive". Mostrá la cantidad de vivos encontrada y sus nombres.
-//Mostrá: "Personajes vivos en esta página: X" y luego los nombres
-
-fetch("https://rickandmortyapi.com/api/character")
-    .then (response => response.json())
-    .then (data => {
-        const personajesVivos = data.results.filter(personaje => personaje.status === "Alive");
-        document.getElementById("resultado").innerHTML = `Personajes vivos en esta paginas: ${personajesVivos.length} <br> Nombres: ${personajesVivos.map(personaje => personaje.name).join(" --- ")}`;
-    });
